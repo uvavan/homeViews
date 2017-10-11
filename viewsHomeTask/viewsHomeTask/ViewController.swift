8b22b3efb4be1214d204a7f49e8625975b98f7a2
Admin <vitaliy.kazakoff2013@gmail.com>
2017-10-11 21:31:50 +0300

    end roject

--- viewsHomeTask/viewsHomeTask/ViewController.swift
+++ viewsHomeTask/viewsHomeTask/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ibImageViewOne: UIImageView!
+    @IBOutlet weak var ibImageViewTwo: UIImageView!
+    @IBOutlet weak var ibImageViewTree: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func ibButtonShowAllRees(_ sender: Any) {
+        ibImageViewOne.isHidden = false
+        ibImageViewTwo.isHidden = false
+        ibImageViewTree.isHidden = false
     }
-
-
+    
+    @IBAction func ibButtonOneRees(_ sender: Any) {
+        ibImageViewOne.isHidden = true
+    }
+    
+    @IBAction func ibButtonTwoRees(_ sender: Any) {
+        ibImageViewTwo.isHidden = true
+    }
+    
+    @IBAction func ibButtonTreeRees(_ sender: Any) {
+        ibImageViewTree.isHidden = true
+    }
+    
 }
 
